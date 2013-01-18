45d6ab49941a74cb21b0d2bbb77bd736a67c3953
Valentin Kolesnikov <javadev75@gmail.com>
2013-01-18 07:52:56 +0200

    Added license

--- src/main/java/com/github/xmltopdf/JasperPdfGenerator.java
+++ src/main/java/com/github/xmltopdf/JasperPdfGenerator.java
@@ -1,4 +1,21 @@
-package com.github.jasperxmltopdfgenerator;
+/*
+ * $Id$
+ *
+ * Copyright 2013 Valentyn Kolesnikov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.xmltopdf;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -29,6 +46,10 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRPrintPage;
 
+/**.
+ * @author Valentyn Kolesnikov
+ * @version $Revision$ $Date$
+ */
 public class JasperPdfGenerator {
     private XMLTag xmlTag;
 
